190176b009e62752ba6f3587bdbac7d805c09236
Bhaskar Govil <bhaskar.govil.14@gmail.com>
2024-06-10 03:47:54 +0530

    deployment changes made to the website

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { connectionDB } from "./db/connect.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 configDotenv({ path: "./.env" });
 
@@ -14,19 +15,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-connectionDB()
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on ${process.env.PORT}`);
-    });
-  })
-  .catch((e) => {
-    throw e;
-  });
-
-app.get("/", (req, res) => {
-  res.send(`Server is running on ${process.env.PORT}`);
-});
+const __dirname = path.resolve();
 
 // importing all the routes
 import authRouter from "./routes/auth.route.js";
@@ -39,3 +28,16 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/notifications", notificationRouter);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
+  connectionDB();
+});
