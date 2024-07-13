774ccbe7995966a64bb429107af2370a3edb5423
Bhaskar Govil <bhaskar.govil.14@gmail.com>
2024-07-13 23:47:22 +0530

    putting dist folder from frontend dir to home dir

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,10 @@ app.use("/api/post", postRouter);
 app.use("/api/notifications", notificationRouter);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 }
 
